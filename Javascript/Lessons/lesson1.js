3218dd9304ca9c944c37a36eec71573fa5790f5d
MohammedJama <themjama@outlook.com>
2023-07-15 14:16:49 +0100

    first JS lesson

---
+++ Javascript/Lessons/lesson1.js
@@ -0,0 +1,16 @@
+//this is a single line comment
+
+/*
+this is
+a multi
+line
+comment*/
+
+console.log("Hello to be execuded in JS lesson1");
+
+
+var name = "Mohammed";
+
+console.log(name)
+
+console.log(10*10)
