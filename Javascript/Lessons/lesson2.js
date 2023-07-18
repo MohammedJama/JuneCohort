93a51c4504ea5c61109d4fbd6005061305dbb8f4
MohammedJama <themjama@outlook.com>
2023-07-18 20:48:47 +0100

    lesson 2 of JS

---
+++ Javascript/Lessons/lesson2.js
@@ -0,0 +1,40 @@
+let age = 31
+console.log(age)
+const name = "Mohammed"
+
+age = 26;
+console.log(age)
+
+//name = "steve";
+
+console.log(name)
+
+//------------
+
+//string
+//number
+//boolean
+//null
+//undefined
+//object
+
+// array = anything in square bracket ["",1,true,undefine,null,objects] - to be seperated by commas
+
+let array = [ "bread", "milk", "cheese"]
+// position/index != number of times
+// starts from 0 instead of 1
+// Position = n -1
+
+// Push vs Pop last in = first out
+//array.push("item")
+//array.pop("item")
+
+
+array[3] = "Juice"
+array[4] = "Pasta"
+
+array.push("rice")
+
+console.log (array)
+
+//array.splice
