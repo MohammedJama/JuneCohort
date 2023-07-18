23e52b23b102785b2c70ca7cd2ef4a5e5a237506
MohammedJama <themjama@outlook.com>
2023-07-18 19:26:44 +0100

    first JS homework

---
+++ Javascript/Homework/homework1.js
@@ -0,0 +1,30 @@
+// add two different strings 
+
+console.log("Steve"+" "+"Jobs")
+
+
+// create a variable for a number, a string. Then print each of these variables.
+
+var currentMonth = "July"
+
+var currentYear = "2023"
+
+console.log (currentMonth+" "+currentYear)
+
+// combining 3 different variables
+
+console.log ("A few things about me")
+
+var bestTeam = " Arsenal"
+
+var favouriteFood = "nothing beats Bariis iyo Hilib"
+
+var favouiteSeason = "I prefer winter over summer"
+
+console.log ("my favouite team is", bestTeam , "my favourite food is", favouriteFood, "my favourite season is", favouiteSeason)
+
+// Create a variable that hold both numbers and strings and print this off
+
+var islamicDate = "Tomorrow is the start of the new year. Muharram 1445"
+
+console.log (islamicDate)
